1f533e5b8445fe0f5cd5d2a428a473f5fc7c15b0
amaucher <andreas-maucher@gmx.de>
2024-09-03 19:02:03 +0200

    match up error for robotomy request form

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,22 +6,37 @@ Intern::Intern(void) {}
 // Destructor
 Intern::~Intern(void) {}
 
+// exception
+const char *Intern::FormNotThere::what() const throw() 
+{
+	return ("Exception called: Form does not match.");
+}
+
 // public method
 AForm *Intern::makeForm(std::string formName, std::string target) const
-		throw (FormNotThere) {
+		throw (FormNotThere) 
+		{
 			AForm *form;
-			std::string availableForms[] = {"Shrubbery", "Robotomy", "Presidential"};
+			std::string availableForms[] = {"shrubbery creation", "robotomy request", "presidential pardon"};
 			int i = 0;
-			while (i < 3 && formName != availableForms[i]) {
+			while (i < 3 && formName != availableForms[i]) 
+			{
 				i++;
+				std::cout << "round " << i << std::endl;
 			}
-			switch(i) {
+			switch(i) 
+			{
 				case 0:
 						form = new ShrubberyCreationForm(target);
+						std::cout << "case 0" << std::endl;
+						break;
 				case 1:
 						form = new RobotomyRequestForm(target);
+						std::cout << "case 1" << std::endl;
+						break;
 				case 2:
 						form = new PresidentialPardonForm(target);
+						std::cout << "case 2" << std::endl;
 						break;
 				default:
 						std::cout << "Intern can't find the form. Form not part of the known collection. " << std::endl;
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amaucher <amaucher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 14:22:27 by amaucher          #+#    #+#             */
-/*   Updated: 2024/09/01 20:13:05 by amaucher         ###   ########.fr       */
+/*   Updated: 2024/09/03 18:57:09 by amaucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,109 +15,22 @@
 #include "ShrubberyCreationForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
-
-/*
-destructor of abstract base class needs to be virtual if a derived object
-through a base class pointer is supposed to be deleted (relevant for *form)
-*/
+#include "Intern.hpp"
 
 //! change the main
 
 int main(void)
 {
-	AForm *mautForm = new ShrubberyCreationForm("Test 1");
+	Intern jonny;
+	AForm	*sampleForm;
 	try {
-		Bureaucrat scheuer("scheuer", 1);
-		mautForm->beSigned(scheuer);
-		scheuer.signForm(*mautForm);
-		mautForm->execute(scheuer);
+		sampleForm = jonny.makeForm("robotomy request", "1");
 	}
 	catch (std::exception& e) {
-		std::cout << "mautForm:" << e.what() << std::endl;
+		std::cout << "form1: " << e.what() << std::endl;
 	}
-	delete mautForm;
+	delete sampleForm;
 	std::cout << std::endl;
-	mautForm = new ShrubberyCreationForm("test");
-	try {
-		Bureaucrat scheuer("scheuer", 1);
-		mautForm->beSigned(scheuer);
-		scheuer.signForm(*mautForm);
-		scheuer.executeForm(*mautForm);
-	}
-	catch (std::exception& e) {
-		std::cout << "mautForm:" << e.what() << std::endl;
-	}
-	delete mautForm;
-
-	std::cout << std::endl;
-	mautForm = new ShrubberyCreationForm("test");
-	try {
-		Bureaucrat scheuer("scheuer", 1);
-		mautForm->beSigned(scheuer);
-		scheuer.signForm(*mautForm);
-		mautForm->execute(scheuer);
-	}
-	catch (std::exception& e) {
-		std::cout << "mautForm:" << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	delete mautForm;
-
-	mautForm = new RobotomyRequestForm("test");
-	try {
-		Bureaucrat scheuer("scheuer", 1);
-		mautForm->beSigned(scheuer);
-		scheuer.signForm(*mautForm);
-		mautForm->execute(scheuer);
-
-	}
-	catch (std::exception& e) {
-		std::cout << "mautForm:" << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	delete mautForm;
-
-	mautForm = new PresidentialPardonForm("test");
-	try {
-		Bureaucrat scheuer("scheuer", 1);
-		mautForm->beSigned(scheuer);
-		scheuer.signForm(*mautForm);
-		mautForm->execute(scheuer);
-	}
-	catch (std::exception& e) {
-		std::cout << "mautForm:" << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	delete mautForm;
-
-	mautForm = new PresidentialPardonForm("test");
-	try {
-		Bureaucrat scheuer("scheuer", 25);
-		std::cout << scheuer << std::endl;
-		std::cout << *mautForm << std::endl;
-		mautForm->beSigned(scheuer);
-		scheuer.signForm(*mautForm);
-		mautForm->execute(scheuer);
-	}
-	catch (std::exception& e) {
-		std::cout << "mautForm:" << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	delete mautForm;
-
-	mautForm = new ShrubberyCreationForm("test");
-	try {
-		Bureaucrat scheuer("scheuer", 146);
-		std::cout << scheuer << std::endl;
-		std::cout << *mautForm << std::endl;
-		mautForm->beSigned(scheuer);
-		scheuer.signForm(*mautForm);
-		mautForm->execute(scheuer);
-	}
-	catch (std::exception& e) {
-		std::cout << "mautForm:" << e.what() << std::endl;
-	}
-	delete mautForm;
-	return 0;
+	
 }
 
