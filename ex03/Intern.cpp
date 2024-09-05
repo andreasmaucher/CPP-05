d8be658c65ec20743f0021eb89bd003188422bcc
amaucher <andreas-maucher@gmx.de>
2024-09-05 17:38:36 +0200

    ex03 done

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -22,21 +22,17 @@ AForm *Intern::makeForm(std::string formName, std::string target) const
 			while (i < 3 && formName != availableForms[i]) 
 			{
 				i++;
-				std::cout << "round " << i << std::endl;
 			}
 			switch(i) 
 			{
 				case 0:
 						form = new ShrubberyCreationForm(target);
-						std::cout << "case 0" << std::endl;
 						break;
 				case 1:
 						form = new RobotomyRequestForm(target);
-						std::cout << "case 1" << std::endl;
 						break;
 				case 2:
 						form = new PresidentialPardonForm(target);
-						std::cout << "case 2" << std::endl;
 						break;
 				default:
 						std::cout << "Intern can't find the form. Form not part of the known collection. " << std::endl;
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amaucher <amaucher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 14:22:27 by amaucher          #+#    #+#             */
-/*   Updated: 2024/09/03 18:57:09 by amaucher         ###   ########.fr       */
+/*   Updated: 2024/09/05 17:34:49 by amaucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,57 @@
 #include "RobotomyRequestForm.hpp"
 #include "Intern.hpp"
 
-//! change the main
+/*
+After each deletion, you need to reset sampleForm to nullptr 
+to prevent re-deleting an already deleted pointer.
+*/
 
 int main(void)
 {
 	Intern jonny;
-	AForm	*sampleForm;
+	AForm	*sampleForm = NULL;
 	try {
-		sampleForm = jonny.makeForm("robotomy request", "1");
+		sampleForm = jonny.makeForm("robotomy request", "Bender");
+		std::cout << *sampleForm << std::endl;
 	}
 	catch (std::exception& e) {
 		std::cout << "form1: " << e.what() << std::endl;
 	}
 	delete sampleForm;
+	sampleForm = NULL;
 	std::cout << std::endl;
-	
+	try {
+		sampleForm = jonny.makeForm("shrubbery creation", "Bender");
+		std::cout << *sampleForm << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cout << "form2: " << e.what() << std::endl;
+	}
+	delete sampleForm;
+	sampleForm = NULL;
+	std::cout << std::endl;
+	try {
+		sampleForm = jonny.makeForm("presidential pardon", "Bender");
+		std::cout << *sampleForm << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cout << "form3: " << e.what() << std::endl;
+	}
+	delete sampleForm;
+	sampleForm = NULL;
+	std::cout << std::endl;
+	try {
+		sampleForm = jonny.makeForm("president pardon", "Bender");
+	}
+	catch (std::exception& e) {
+		std::cout << "form4: " << e.what() << std::endl;
+	}
+	if (sampleForm)
+	{
+		delete sampleForm;	
+		sampleForm = NULL;
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
 }
 
